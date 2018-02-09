d01e1fa2f12a792ab1931c81260baff71d4e17dc
GPRUNNER2000 <ysptoyrunner@gmail.com>
2018-02-09 08:36:18 -0800

    More testing
    - Modified tester.java

--- src/tester.java
+++ src/tester.java
@@ -1,5 +1,8 @@
 public class tester {
     public static void main(String[] args){
         System.out.println("Test");
+        for (int i = 12; i >= 0; i--){
+            System.out.print(i + " ");
+        }
     }
 }
